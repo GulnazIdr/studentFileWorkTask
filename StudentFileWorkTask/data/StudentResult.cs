13ccd6d167fa64162dcc9f3241a55c71e420fe58
GulnazIdr <gulnazidr17@gmail.com>
2026-05-22 23:34:08 +0300

    filtering and sorting

--- StudentFileWorkTask/data/StudentResult.cs
+++ StudentFileWorkTask/data/StudentResult.cs
@@ -8,15 +8,13 @@ namespace StudentFileWorkTask.data
 {
     internal class StudentResult
     {
-        public Theme Theme { get; set; }
-        public string Question { get; set; } = string.Empty;
-        public int Score { get; set; }
         public Student Student { get; set; }
+        public double Score { get; set; }
+        public Question Question { get; set; } 
         public DateOnly? Date { get; set; } = null;
 
-        public StudentResult(Student student, Theme theme, int score, string question = "", DateOnly? date = null)
+        public StudentResult(Student student, Question question, double score,  DateOnly? date = null)
         {
-            Theme = theme;
             Question = question;
             Score = score;
             Date = date;
--- StudentFileWorkTask/data/StudentResultFilter.cs
+++ StudentFileWorkTask/data/StudentResultFilter.cs
@@ -9,11 +9,11 @@ namespace StudentFileWorkTask.data
     internal class StudentResultFilter
     {
         public string FilterName { get; set; }
-        public List<Filter> OptionList { get; set; }
+        public List<Filter> Options { get; set; }
         public StudentResultFilter(string filterName, List<Filter> optionList)
         {
             FilterName = filterName;
-            OptionList = optionList;
+            Options = optionList;
         }
     }
 }
