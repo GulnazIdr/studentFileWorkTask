4bb356362b55a5ce5436661f1e9c47cab51b3143
thedetsel <139446173+bluelord1469@users.noreply.github.com>
2026-06-01 03:00:53 +0300

    Revert "Export"

--- StudentFileWorkTask/obj/Debug/net6.0-windows/StudentFileWorkTask.AssemblyInfo.cs
+++ StudentFileWorkTask/obj/Debug/net6.0-windows/StudentFileWorkTask.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentFileWorkTask")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+528a0a61904d5345ef75fb3d8eae0d10576591ff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11f128f2410fbad607028c2bf6562fe6eba10b79")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentFileWorkTask")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudentFileWorkTask")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- StudentFileWorkTask/presentation/StudentDataWindow.xaml.cs
+++ StudentFileWorkTask/presentation/StudentDataWindow.xaml.cs
@@ -1,6 +1,4 @@
-﻿using StudentFileWorkTask.data;
-using StudentFileWorkTask.export;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,19 +11,18 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using StudentFileWorkTask.data;
 
 namespace StudentFileWorkTask.presentation
 {
     public partial class StudentDataWindow : Window
     {
         StudentResultViewModel studentResultViewModel;
-        private ExcelExportService exportService;
         public StudentDataWindow()
         {
             InitializeComponent();
             studentResultViewModel = new StudentResultViewModel();
             DataContext = studentResultViewModel;
-            exportService = new ExcelExportService(studentResultViewModel);
         }
 
         private void filterCheck_Checked(object sender, RoutedEventArgs e)
@@ -64,20 +61,43 @@ namespace StudentFileWorkTask.presentation
 
         private void excelCreateBtn_Click(object sender, RoutedEventArgs e)
         {
-            var result = MessageBox.Show(
-                "Выберите действие:\nДа - Создать новый отчет\nНет - Обновить существующий",
-                "Экспорт в Excel",
-                MessageBoxButton.YesNo,
-                MessageBoxImage.Question);
+            var dataGrid = new DataGrid();
+            dataGrid.AutoGenerateColumns = false;
+            dataGrid.Margin = new Thickness(10);
+            dataGrid.Height = 400;
 
-            if (result == MessageBoxResult.Yes)
+            List<Theme> themeList = studentResultViewModel.ThemeList.ToList();
+            dataGrid.Columns.Add(new DataGridTextColumn()
             {
-                exportService.CreateNewReport();
-            }
-            else if (result == MessageBoxResult.No)
+                Header = "№",
+                Binding = new Binding("Index")
+            });
+            dataGrid.Columns.Add(new DataGridTextColumn()
             {
-                exportService.UpdateExistingReport();
+                Header = "ФИО",
+                Binding = new Binding("Student.Surname")
+            });
+
+            for (var i = 0; i < themeList.Count; i++)
+            {
+                var theme = themeList[i].ThemeName;
+                dataGrid.Columns.Add(new DataGridTextColumn()
+                {
+                    Header = theme,
+                    Binding = new Binding($"[{theme}]")
+                });
             }
+
+            dataGrid.Columns.Add(new DataGridTextColumn()
+            {
+                Header = "Сумма баллов",
+                Binding = new Binding("ScoreSummary")
+            });
+
+            List<StudentResultThemeSum> studentResultThemeSumList = studentResultViewModel.GetStudentResultThemeSummary();
+            dataGrid.ItemsSource = studentResultThemeSumList;    
+
+            dataPanel.Children.Add(dataGrid);
         }
     }
 }
