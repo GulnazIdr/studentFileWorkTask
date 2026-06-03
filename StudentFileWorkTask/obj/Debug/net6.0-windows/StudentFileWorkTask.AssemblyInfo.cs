8770f89aa62bb24ef54be7044ea92d62304d92ab
GulnazIdr <gulnazidr17@gmail.com>
2026-06-03 14:49:35 +0300

    uncommited

--- StudentFileWorkTask/obj/Debug/net6.0-windows/StudentFileWorkTask.AssemblyInfo.cs
+++ StudentFileWorkTask/obj/Debug/net6.0-windows/StudentFileWorkTask.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentFileWorkTask")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+218acc9c8046bcfe7c5a6ce45aec69e87fd7600c")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentFileWorkTask")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudentFileWorkTask")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
